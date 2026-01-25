261c0aca78772a3ced0b58c697d0e05c4209e55f
darkenov <skilbot15@gmail.com>
2026-01-25 22:37:47 +0500

    modify string example

--- examples/string.py
+++ examples/string.py
@@ -1,5 +1,10 @@
+#You can assign a multiline string to a variable by using three quotes:
 a = '''Lorem ipsum dolor sit amet,
 consectetur adipiscing elit,
 sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua.'''
 print(a)
+
+#usual string:
+b="Hello world"
+print(b)
