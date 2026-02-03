75b231e5947a855130568fa3332ad19524e54957
darkenov <skilbot15@gmail.com>
2026-02-03 09:34:49 +0500

    Exapmles of basic functions

---
+++ pracrice3/functions/basic_functions.py
@@ -0,0 +1,25 @@
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
+
+def addition():
+    a=int(input())
+    b=int(input())
+    print(a+b)
+addition()
+
+def upper():
+    print(input().upper())
+upper()
+
+def test():
+    print(input().isalpha())
+test()
